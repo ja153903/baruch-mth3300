aeb29758bbdd13593e591834bfce236a3bf4c6d3
ja153903 <jaimeabbariao@gmail.com>
2025-01-26 02:14:11 -0500

    removing file that we wont be using

--- spr2025/introduction/add.py
+++
@@ -1,9 +0,0 @@
-def add(a: int, b: int) -> int:
-    return a + b
-
-
-if __name__ == "__main__":
-    a = int(input("Enter a number: "))
-    b = int(input("Enter another number: "))
-
-    print(f"The sum of {a} and {b} is {add(a, b)}")
